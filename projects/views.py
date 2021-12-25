4e9d8c214922c43259175acdcbe5a828793b6199
Kimutai01 <kiprotichkimutai01@gmail.com>
2021-12-25 02:32:55 -0500

    adding the templates

--- projects/views.py
+++ projects/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def projects(request):
-    return HttpResponse('This are the projects')
+    return render(request, 'projects.html')
 
 def project(request, pk):
-    return HttpResponse('this is a single project' + ' ' + str(pk))
+    return render(request, 'single-project.html')
