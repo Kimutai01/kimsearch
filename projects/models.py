490366e6526c93034e53786324cb9323cfde42f1
Kimutai01 <kiprotichkimutai01@gmail.com>
2021-12-25 08:44:04 -0500

    adding the string method

--- projects/models.py
+++ projects/models.py
@@ -9,4 +9,7 @@ class Project(models.Model):
     demo_link = models.CharField(max_length=2000, null=True, blank=True)
     source_link = models.CharField(max_length=2000, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
-    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable = False) 
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable = False)
+    
+    def __str__(self):
+        return self.title
