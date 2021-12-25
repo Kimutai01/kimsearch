8975e78b44848e8dfc378ce0dd82fdd5b02c9a38
Kimutai01 <kiprotichkimutai01@gmail.com>
2021-12-25 09:08:11 -0500

    adding the review model and the database relationship

--- projects/models.py
+++ projects/models.py
@@ -2,6 +2,8 @@ from enum import unique
 from django.db import models
 import uuid
 
+from django.db.models.expressions import Value
+
 
 class Project(models.Model):
     title = models.CharField(max_length=200)
@@ -13,3 +15,22 @@ class Project(models.Model):
     
     def __str__(self):
         return self.title
+    
+    
+class Review(models.Model):
+    VOTE_TYPE = (
+        ('up', 'Up Vote'),
+        ('down', 'Down Vote'),
+    )
+    #owner=
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    body = models.TextField(null=True, blank=True)
+    value =  models.CharField(max_length=200, choices = VOTE_TYPE)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable = False)
+    
+    def __str__(self):
+        return self.value
+    
+
+class
