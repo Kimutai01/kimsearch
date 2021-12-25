3ff4f3ad0ed0761f843181c604fef4d36583c583
Kimutai01 <kiprotichkimutai01@gmail.com>
2021-12-25 09:12:38 -0500

    adding the review table

--- projects/models.py
+++ projects/models.py
@@ -10,6 +10,8 @@ class Project(models.Model):
     description = models.TextField(null=True, blank=True)
     demo_link = models.CharField(max_length=2000, null=True, blank=True)
     source_link = models.CharField(max_length=2000, null=True, blank=True)
+    tags = models.ManyToManyField('Tag', blank=True)
+    vote_total = models.IntegerField(default=0, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable = False)
     
@@ -33,4 +35,11 @@ class Review(models.Model):
         return self.value
     
 
-class
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable = False)
+    
+    
+    def __str__(self):
+        return self.value
