09a09f88826b3ae4becc4b85af014c9c71616050
Kimutai01 <kiprotichkimutai01@gmail.com>
2021-12-25 02:40:51 -0500

    adding the projects app template

--- projects/views.py
+++ projects/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def projects(request):
-    return render(request, 'projects.html')
+    return render(request, 'projects/projects.html')
 
 def project(request, pk):
-    return render(request, 'single-project.html')
+    return render(request, 'projects/single-project.html')
