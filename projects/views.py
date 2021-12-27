038e841a7cdb17db0a0690bdf51c65f7df0aa685
Kimutai01 <kiprotichkimutai01@gmail.com>
2021-12-27 11:42:56 -0500

    adding the forms

--- projects/views.py
+++ projects/views.py
@@ -1,24 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Project
+from .forms import ProjectForm
 
-projectsList = [
-        {
-        'id':'1',
-        'title': 'Ecommerce website',
-        'description': 'fully functional ecommerce website'
-        },
-    {
-        'id':'2',
-        'title': 'portfolio  website',
-        'description': 'About me'
-    },
-    {
-        'id':'3',
-        'title': 'loan management website',
-        'description': 'website meant for a sacco'
-    },
-]
 
 def projects(request):
     projects = Project.objects.all()
@@ -30,3 +14,9 @@ def project(request, pk):
     projectObj = Project.objects.get(id=pk)
     tags = projectObj.tags.all()
     return render(request, 'projects/single-project.html', {'project':projectObj, 'tags':tags})
+
+
+def createProject(request):
+    form = ProjectForm()
+    context={'form':form}
+    return render(request, 'projects/project_form.html', context)
