fd43227b93cc663fa9737fcdf86ee3a0617c576e
Kimutai01 <kiprotichkimutai01@gmail.com>
2021-12-28 07:45:09 -0500

    adding the static files in production

--- kimsearch/settings.py
+++ kimsearch/settings.py
@@ -121,12 +121,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- kimsearch/urls.py
+++ kimsearch/urls.py
@@ -1,6 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path , include
+from django.conf import settings
+from django.conf.urls.static import static
     
     
 
@@ -9,3 +11,5 @@ urlpatterns = [
     path('', include('projects.urls'))
     
 ]
+
+urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
