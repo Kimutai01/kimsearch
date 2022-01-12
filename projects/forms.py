f617ff86d00b5b2df582fa9ec5f4be66b25cb542
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-01-12 15:38:38 -0500

    Add widgets on the form

--- projects/forms.py
+++ projects/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Project
 
 
@@ -6,3 +7,14 @@ class ProjectForm(ModelForm):
     class Meta:
         model = Project
         fields = ['title', 'featured_image', 'description', 'demo_link', 'source_link', 'tags']
+        widgets = {
+            'tags':forms.CheckboxSelectMultiple()
+        }
+    
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm,self).__init__(*args, **kwargs)
+        
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class':'input'})
+        
+        # self.fields['title'].widget.attrs.update({'class':'input',})
