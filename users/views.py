8f93a531695411baeb14996a45b7057c3b57f680
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-01-13 04:09:52 -0500

    Add user-profile view

--- users/views.py
+++ users/views.py
@@ -7,3 +7,7 @@ def profiles(request):
     profiles = Profile.objects.all()
     context = {'profiles':profiles}
     return render(request, 'users/profiles.html', context)
+
+
+def userProfile(request, pk):
+    return render(request, 'users/user-profile.html')
