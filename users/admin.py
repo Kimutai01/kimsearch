f02c297efc62f6757a1fecb4c56e9cf40a726f89
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-01-13 03:57:02 -0500

    Display skill in the profiles page

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, Skill
 
 
 # Register your models here.
 
 admin.site.register(Profile)
+admin.site.register(Skill)
+
 
 
