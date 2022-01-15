6a7b3ff87e27b1fe227e383c69c3657579e4a9b8
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-01-15 04:59:49 -0500

    Add signals

--- users/models.py
+++ users/models.py
@@ -1,3 +1,5 @@
+import email
+from unicodedata import name
 from django.db import models
 from django.contrib.auth.models import User
 import uuid
@@ -5,6 +7,8 @@ import uuid
 from django.db.models.base import Model
 
 # Create your models here.
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
@@ -26,7 +30,7 @@ class Profile(models.Model):
     
     
     def __str__(self):
-        return str(self.user.username)
+        return str(self.username)
     
 class Skill(models.Model):
     owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
@@ -37,5 +41,6 @@ class Skill(models.Model):
     
     def __str__(self):
         return str(self.name)
+
     
     
