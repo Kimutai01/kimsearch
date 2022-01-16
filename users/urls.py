26edc774c283b90184f96c60634a2b0aab5eb388
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-01-16 11:56:53 -0500

    Add login and logout functionality

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
     path('', views.profiles, name='profiles'),
     path('profile/<str:pk>/', views.userProfile, name='user-profile'),
+    
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import login, authenticate,logout
+from django.contrib.auth.models import User
 from .models import Profile
 
 # Create your views here.
 
 def loginPage(request):
+    
+    if request.user.is_authenticated:
+        return redirect('profiles')
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        try:
+            user = User.objects.get(username=username)
+        except:
+            print('username does not exist')
+        user = authenticate(request, username=username, password = password)
+        
+        if user is not None:
+            login(request,user)
+            return redirect('profiles')
+        
+        else:
+            print('Username or password incorrect')
     return render(request,'users/login_registry.html')
+
+def logoutUser(request):
+    return redirect('login')
 
 def profiles(request):
     profiles = Profile.objects.all()
